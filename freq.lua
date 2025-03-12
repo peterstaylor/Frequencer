2c793bb940e8f644c9cd71e4001a6efa4a527618
peterstaylor <31700136+peterstaylor@users.noreply.github.com>
2025-03-12 15:56:22 -0400

    playing around with envelope detection

--- freq.lua
+++ freq.lua
@@ -9,10 +9,12 @@ counter = 1
 last = 440
 averaged = 0
 prevOutput = 0
+h2vref = 58.14
 
 function init()
     input[1].mode('freq',0.001)
-    input[2].mode('change',1,0.05,'rising')
+    --input[2].mode('change',1,0.05,'rising')
+    input[2].mode('volume', 0.01)
     output[1].scale({})
     output[2].scale({})
     output[3].slew = 1
@@ -27,20 +29,35 @@ input[1].freq = function(freq)
     output[4].volts = map(accum / counter)
 end
 
-input[2].change = function(s)
-    averaged = accum / counter
-    counter = 1
-    accum = last
-    diff = math.abs(prevOutput - averaged)
-    prevOutput = averaged
-    v1 = map(last)
-    v2 = map(averaged)
-    output[1].volts = v1
-    output[2].volts = v2
-    
+input[2].volume = function(vol)
+    print(vol)
+    if vol > volThresh then 
+        averaged = accum / counter
+        counter = 1
+        accum = last
+        diff = math.abs(prevOutput - averaged)
+        prevOutput = averaged
+        v1 = map(last)
+        v2 = map(averaged)
+        output[1].volts = v1
+        output[2].volts = v2 
+    end
 end
 
+--input[2].change = function(s)
+--    averaged = accum / counter
+--    counter = 1
+--    accum = last
+--    diff = math.abs(prevOutput - averaged)
+--    prevOutput = averaged
+--    v1 = map(last)
+--    v2 = map(averaged)
+--    output[1].volts = v1
+--    output[2].volts = v2    
+--end
+
 function map(value)
-    volt = -5.81 + 1.43 * math.log(value)
+    volt = hztovolts(value [,h2vref])
+    --volt = -5.81 + 1.43 * math.log(value)
     return volt
 end
