a0ebaf459f06c4ad1d6ade360018f7a93c386bdc
peterstaylor <31700136+peterstaylor@users.noreply.github.com>
2025-04-02 09:27:55 -0400

    simplifying code, adding slewed frequency output

--- freq.lua
+++ freq.lua
@@ -1,7 +1,7 @@
 --- Frequencer
 -- in1: Any Audio Input
 -- in2: Any Audio Input, theoreitcally same as in1
--- out1: Last Detected Pitch
+-- out1: Slewing Detected Pitch
 -- out2: Accumulated Detected Pitch
 -- out3: Envelope Follower
 -- out4: gate from volume detection
@@ -13,12 +13,11 @@ averaged = 0
 h2vref = 58.14 -- experimentally derived
 volThresh = 2
 volgate = 0
-follower = {}
+timeForEF = 0.25
 volumeSR = 0.01 
-followerIndex = 1
 highVoltage = 7
 retrigdelay = 0 
-retrigcap = 50
+retrigcap = 20
 
 function init()
     input[1].mode('freq',0.001)
@@ -26,29 +25,20 @@ function init()
     output[1].scale({})
     output[2].scale({})
     output[4].action = pulse(0.001, 7, 1)
-
-    -- initialize envelope follower array to contain last second of volume readings
-    for i = 1, (1/volumeSR) do
-        follower[i] = 0 
-    end 
+   
 end
 
 input[1].freq = function(freq)
-    freq = freq/2
     last = freq
+    output[1].slew = 0.1
+    output[1].volts = map(last)
     accum = accum + freq
     counter = counter + 1
 end
 
 input[2].volume = function(vol)
-    
-    -- update envelope follower array and calculate rolling average
-    follower[followerIndex] = vol
-    followerIndex = followerIndex + 1
-    if followerIndex > (1/volumeSR) then
-        followerIndex = 1
-    end
-    output[3].volts = averageEnvelope(follower)
+    output[3].slew = 0.1
+    output[3].volts = vol
 
     if volgate == 1 then
         retrigdelay = retrigdelay + 1
@@ -57,13 +47,9 @@ input[2].volume = function(vol)
     -- check if threshold has been exceeded and upate gate and pitches
     if vol > volThresh and volgate == 0 then 
         averaged = accum / counter
-        print(averaged)
         counter = 1
         accum = last
-        v1 = map(last)
         v2 = map(averaged)
-        
-        output[1].volts = v1
         output[2].volts = v2 
         output[4]()
         volgate = 1
@@ -78,15 +64,4 @@ function map(value)
     --volt = hztovolts(value [h2vref])
     volt = -5.81 + 1.43 * math.log(value)
     return volt
-end
-
--- calculates average volume reading over the last second for EF
-function averageEnvelope(array)
-    pile = 0
-    for i = 1, 1/volumeSR do
-        pile = pile + array[i]
-    end
-    
-    average = pile * volumeSR
-    return average
 end
