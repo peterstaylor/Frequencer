2cc8d2e775bbc57bca41b9167c6b32b8e725708a
peterstaylor <31700136+peterstaylor@users.noreply.github.com>
2025-04-03 19:39:05 -0400

    signifcant clean up, switch to average arrays

--- freq.lua
+++ freq.lua
@@ -1,46 +1,59 @@
 --- Frequencer
 -- in1: Any Audio Input
 -- in2: Any Audio Input, theoreitcally same as in1
--- out1: Slewing Detected Pitch
--- out2: Accumulated Detected Pitch
--- out3: Envelope Follower
--- out4: gate from volume detection
+-- out1: Wider Time Window Detected Pitch, Rolling Avg
+-- out2: Smaller Time Window Detected Pitch, Output on Volume Thresh
+-- out3: Envelope Follower of Input
+-- out4: Gate from Volume Threshold of Audio Input
 
-accum = 0
-counter = 1
-last = 440
-averaged = 0
 h2vref = 58.14 -- experimentally derived
 volThresh = 2
 volgate = 0
-timeForEF = 0.25
 volumeSR = 0.01 
-freqSR = 0.001
-highVoltage = 7
-slewCount = 0
-freqAvg = {}
+freqSR = 0.05
+
+-- these variables are used to create a running average
+-- of the frequency detector output
+slowF = 10  -- in seconds
+slowCounter = 1
+fastF = 0.5 -- in seconds
+fastCounter = 1
+slowLen = slowF / freqSR
+fastLen = fastF / freqSR
+slowAvg = {}
+fastAvg = {}
 
 function init()
     input[1].mode('freq',freqSR)
     input[2].mode('volume', volumeSR)
     output[1].scale({0,9,7,9,4})
     output[2].scale({0,2,4,5,7,9,11})
-    output[4].action = pulse(0.01, 7, 1)
-   
+
+    for i = 1, slowLen do
+        slowAvg[i] = 0
+    end
+
+    for i = 1, fastLen do
+        fastAvg[i] = 0
+    end
 end
 
 input[1].freq = function(freq)
-    slewCount = slewCount + 1
-    
-    last = freq
-    if slewCount > (1/freqSR) then
-        slewCount = 0 
-        output[1].slew = 2
-        go = map(freq/2)
-        output[1].volts = go
+    slowAvg[slowCounter] = freq
+    fastAvg[fastCounter] = freq
+
+    slowCounter = slowCounter + 1
+    fastCounter = fastCounter + 1
+
+    if slowCounter > slowLen then
+        slowCounter = 1
     end
-    accum = accum + freq
-    counter = counter + 1
+
+    if fastCounter > fastLen then
+        fastCounter = 1
+    end
+
+    output[1].volts = map(averageArray(slowAvg, slowLen))
 end
 
 input[2].volume = function(vol)
@@ -49,14 +62,11 @@ input[2].volume = function(vol)
 
     -- check if threshold has been exceeded and upate gate and pitches
     if vol > volThresh and volgate == 0 then 
-        averaged = accum / counter
-        counter = 1
-        accum = last
-        v2 = map(averaged/2)
-        output[2].volts = v2 
-        output[4]()
+        output[2].volts = map(averageArray(fastAvg, fastLen))
+        output[4].volts = 7
         volgate = 1
-    elseif (vol <= volThresh and volgate == 1) then
+    elseif vol <= volThresh and volgate == 1 then
+        output[4].volts = 0
         volgate = 0
     end
 end
@@ -67,3 +77,12 @@ function map(value)
     volt = -5.81 + 1.43 * math.log(value)
     return volt
 end
+
+function averageArray(array, len)
+    pile = 0
+    for i = 1, len do
+        pile = pile + array[i]
+    end
+    return pile / len
+end
+
